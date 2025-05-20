9ed3b59881504e330249f14425eaeea80697206a
Linoa <linoa.judd@protonmail.com>
2025-05-20 17:57:44 +0200

    v1.1 NULL protection strlen and strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ljudd <ljudd@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 15:28:17 by ljudd             #+#    #+#             */
-/*   Updated: 2025/04/25 14:03:45 by ljudd            ###   ########.fr       */
+/*   Updated: 2025/05/20 17:57:17 by ljudd            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	k;
 
+	if (!s1 && !s2)
+		return (0);
+	if (!s1)
+		return (-1);
+	if (!s2)
+		return (1);
 	k = 0;
 	while (s1[k] && s2[k] && k < n)
 	{
